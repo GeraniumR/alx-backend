41fb5135bebd9ee33e7695fdd98898c69226fe72
GeraniumR <rozanowino@gmail.com>
2023-04-20 00:09:01 +0300

    caches

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""  Basic in dictionary """
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """ Class that inherits from BaseCaching and is a caching system
+        This caching system does not have limit """
+    def put(self, key, item):
+        """ Assign to the dictionary """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Return the value linked """
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        return self.cache_data.get(key)
